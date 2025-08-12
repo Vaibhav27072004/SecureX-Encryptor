714fe82a96cbf64b9fc44e49a7b6d5857c735ce2
Vaibhav Kumar Sahu <117654137+Vaibhav27072004@users.noreply.github.com>
2025-08-12 09:45:44 +0530

    Updating message_encryptor.py

--- message_encryptor.py
+++ message_encryptor.py
@@ -3,10 +3,46 @@ from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.fernet import Fernet
 import hmac
 import hashlib
+import base64
+import time
+from colorama import Fore, Style, init
+import os
+
+init(autoreset=True) 
 
 
-# 1. Generate RSA Keys
+
+def banner():
+    os.system("cls" if os.name == "nt" else "clear")
+    print(Fore.CYAN + Style.BRIGHT + """
+   _____                      _   __  __
+  / ____|                    | | |  \/  |
+ | (___   ___  ___ _   _ _ __| |_| \  / | ___
+  \___ \ / _ \/ __| | | | '__| __| |\/| |/ _ \\
+  ____) |  __/ (__| |_| | |  | |_| |  | |  __/
+ |_____/ \___|\___|\__,_|_|   \__|_|  |_|\___|
+
+    """ + Fore.YELLOW + ">> SecureX Encryption Suite <<\n")
+
+
+def slow_print(text, delay=0.02):
+    for char in text:
+        print(char, end="", flush=True)
+        time.sleep(delay)
+    print()
+
+
+def loading(message, dots=3):
+    print(Fore.YELLOW + message, end="", flush=True)
+    for _ in range(dots):
+        print(".", end="", flush=True)
+        time.sleep(0.4)
+    print()
+
+
+
 def generate_rsa_key():
+    loading("Generating RSA Keys")
     private_key = rsa.generate_private_key(
         public_exponent=65537,
         key_size=2048
@@ -25,10 +61,9 @@ def generate_rsa_key():
             encoding=serialization.Encoding.PEM,
             format=serialization.PublicFormat.SubjectPublicKeyInfo
         ))
-    print("RSA keys generated and saved as 'private_key.pem' and 'public_key.pem'.")
+    print(Fore.GREEN + "RSA keys generated successfully!")
 
 
-# 2. AES Encryption/Decryption
 def aes_encrypt_message(message):
     aes_key = Fernet.generate_key()
     cipher = Fernet(aes_key)
@@ -41,7 +76,6 @@ def aes_decrypt_message(encrypted_message, aes_key):
     return cipher.decrypt(encrypted_message).decode()
 
 
-# 3. HMAC Functions
 def create_hmac(message, key):
     return hmac.new(key, message, hashlib.sha256).hexdigest()
 
@@ -51,7 +85,6 @@ def verify_hmac(message, key, received_hmac):
     return hmac.compare_digest(calculated_hmac, received_hmac)
 
 
-# 4. RSA Encryption/Decryption
 def rsa_encrypt_key(aes_key, public_key_path):
     with open(public_key_path, "rb") as key_file:
         public_key = serialization.load_pem_public_key(key_file.read())
@@ -85,57 +118,76 @@ def rsa_decrypt_key(encrypted_aes_key, private_key_path):
     return aes_key
 
 
-# 5. Encryption/Decryption Workflow
 def encrypt_message(message, public_key_path, hmac_key):
     aes_key, encrypted_message = aes_encrypt_message(message)
     message_hmac = create_hmac(encrypted_message, hmac_key)
     encrypted_aes_key = rsa_encrypt_key(aes_key, public_key_path)
-    return encrypted_message, message_hmac, encrypted_aes_key
+
+    return (
+        base64.b64encode(encrypted_message).decode(),
+        message_hmac,
+        base64.b64encode(encrypted_aes_key).decode()
+    )
 
 
-def decrypt_message(encrypted_message, encrypted_aes_key, private_key_path, hmac_key, received_hmac):
+def decrypt_message(encrypted_message_b64, encrypted_aes_key_b64, private_key_path, hmac_key, received_hmac):
+    encrypted_message = base64.b64decode(encrypted_message_b64)
+    encrypted_aes_key = base64.b64decode(encrypted_aes_key_b64)
+
     aes_key = rsa_decrypt_key(encrypted_aes_key, private_key_path)
 
-    # Verify HMAC
     if not verify_hmac(encrypted_message, hmac_key, received_hmac):
         raise ValueError("Message integrity check failed. Possible tampering detected!")
 
-    # Decrypt the message
     return aes_decrypt_message(encrypted_message, aes_key)
 
 
-# Main Program
+
 if __name__ == "__main__":
-    print("1. Generate RSA Keys")
-    print("2. Encrypt a Message")
-    print("3. Decrypt a Message")
-    choice = input("Enter your choice: ")
+    while True:
+        banner()
+        print(Fore.CYAN + "1." + Fore.WHITE + " Generate RSA Keys")
+        print(Fore.CYAN + "2." + Fore.WHITE + " Encrypt a Message")
+        print(Fore.CYAN + "3." + Fore.WHITE + " Decrypt a Message")
+        print(Fore.CYAN + "4." + Fore.WHITE + " Exit")
+        choice = input(Fore.YELLOW + "\nEnter your choice: " + Fore.WHITE)
 
-    if choice == "1":
-        generate_rsa_key()
+        if choice == "1":
+            generate_rsa_key()
+            input(Fore.YELLOW + "\nPress Enter to continue...")
 
-    elif choice == "2":
-        public_key_path = "public_key.pem"
-        message = input("Enter the message to encrypt: ")
-        hmac_key = input("Enter a secret HMAC passphrase: ").encode()
+        elif choice == "2":
+            public_key_path = "public_key.pem"
+            message = input(Fore.GREEN + "Enter the message to encrypt: " + Fore.WHITE)
+            hmac_key = input(Fore.GREEN + "Enter a secret HMAC passphrase: " + Fore.WHITE).encode()
 
-        encrypted_message, message_hmac, encrypted_aes_key = encrypt_message(message, public_key_path, hmac_key)
-        print("\n\nEncrypted Message: ", encrypted_message)
-        print("\nHMAC :", message_hmac)
-        print("\nEncrypted AES Key:", encrypted_aes_key)
+            loading("Encrypting Message")
+            encrypted_message_b64, message_hmac, encrypted_aes_key_b64 = encrypt_message(message, public_key_path, hmac_key)
 
-    elif choice == "3":
-        private_key_path = "private_key.pem"
-        encrypted_message = eval(input("Enter the encrypted message (as bytes): "))
-        encrypted_aes_key = eval(input("Enter the encrypted AES key: "))
-        hmac_key = input("Enter the secret HMAC passphrase: ").encode()
-        received_hmac = input("Enter the received HMAC: ")
+            print(Fore.CYAN + "\nEncrypted Message (Base64):" + Fore.WHITE, encrypted_message_b64)
+            print(Fore.CYAN + "\nHMAC:" + Fore.WHITE, message_hmac)
+            print(Fore.CYAN + "\nEncrypted AES Key (Base64):" + Fore.WHITE, encrypted_aes_key_b64)
+            input(Fore.YELLOW + "\nPress Enter to continue...")
 
-        try:
-            decrypted_message = decrypt_message(encrypted_message, encrypted_aes_key, private_key_path, hmac_key, received_hmac)
-            print("\n\nDecrypted Message:", decrypted_message)
-        except ValueError as e:
-            print(e)
+        elif choice == "3":
+            private_key_path = "private_key.pem"
+            encrypted_message_b64 = input(Fore.GREEN + "Enter the encrypted message (Base64): " + Fore.WHITE)
+            encrypted_aes_key_b64 = input(Fore.GREEN + "Enter the encrypted AES key (Base64): " + Fore.WHITE)
+            hmac_key = input(Fore.GREEN + "Enter the secret HMAC passphrase: " + Fore.WHITE).encode()
+            received_hmac = input(Fore.GREEN + "Enter the received HMAC: " + Fore.WHITE)
 
-    else:
-        print("Invalid choice.")
+            try:
+                loading("Decrypting Message")
+                decrypted_message = decrypt_message(encrypted_message_b64, encrypted_aes_key_b64, private_key_path, hmac_key, received_hmac)
+                print(Fore.CYAN + "\nDecrypted Message:" + Fore.WHITE, decrypted_message)
+            except ValueError as e:
+                print(Fore.RED + str(e))
+            input(Fore.YELLOW + "\nPress Enter to continue...")
+
+        elif choice == "4":
+            slow_print(Fore.YELLOW + "Exiting SecureX... Goodbye!", 0.03)
+            break
+
+        else:
+            print(Fore.RED + "Invalid choice. Try again!")
+            time.sleep(1)
